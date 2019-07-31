23df4ebc8eec4a65f9da9f102d647d815141dd85
LeenD <52420332+LeenD@users.noreply.github.com>
2019-07-31 18:04:31 +0300

    Leen Aldahmash javascript_intro Hw

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Loaded!");
+
+const firstWord = "Please";
+const secondWord = "squeeze";
+const thirdWord = "the";
+const fourthWord = "cheese";
+
+console.log(firstWord + secondWord + thirdWord + fourthWord);
+
+
+
+const num1 = 5;
+const num2 = 10;
+const num3 = num1 + num2;
+console.log(num3)
