325462bd73f0ff3b13a7416b2601185956f8cc27
Abdulaziz Alsalhi <abdulazizalsalhi@hotmail.com>
2019-07-31 18:47:15 +0300

    Abdualziz Alsalhi submit javacsript intro hw

--- README.md
+++ README.md
@@ -26,15 +26,15 @@ c = a;
 ### Your solution here:
 1.  What is `a`?
 ```
-a is ...
+a is bongos
 ```
 2.  What is `b`?
 ```
-b is ...
+b is true
 ```
 3.  What is `c`?
 ```
-c is ...
+c is 1
 ```
 
 ## Activity Concatenation
@@ -91,14 +91,14 @@ h) 9 !== 8 + 1
 ### Your solution here:
 7.  Write `true` or `false` based on the list above
 ```
-a) 
-b)  
-c)
-d) 
-e) 
-f) 
-g) 
-h) 
+a) false
+b)  true
+c)false
+d) false
+e) false
+f) false
+g) true
+h) false
 ```
 
 ## Activity Conditionals
--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Loaded!");
+
+let a = 1;
+let b = 'bongos';
+let c = true;
+
+a = b;
+b = c;
+c = a;
+
+console.log(c);
+
+const firstWord = "Please";
+const secondWord = "squeeze";
+const thirdWord = "the";
+const fourthWord = "cheese";
+
+console.log(firstWord+" "+secondWord+" "+thirdWord+" "+fourthWord);
+
+
+const num1 = 5;
+const num2 = 10;
+const num3 = num1+num2;
+
+console.log("The sum of"+" "+num1+" "+"and"+" "+num2+" "+"is"+" "+num3)
+console.log(9 !== 8 + 1);
+
+
+function printFizz(number){
+
+    for(var i=0;i<=number;i++){
+        if(i%3==0 && i%5==0){
+            console.log("FizzBuzz");
+        }
+        else if(i%5==0){
+            console.log("Buzz");
+        }
+        else if(i%3==0){
+            console.log("Fizz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+    
+
+}
